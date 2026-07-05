dc9aed9fbd04c38f089b36cadf4475dae0676fcb
S Naveen Kumar <naveensanthanam2006@gmail.com>
2026-07-05 12:51:16 +0530

    Delete start.bat

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting TechDoc backend server...
-cd /d "%~dp0"
-if not exist node_modules (
-  echo Installing dependencies...
-  npm install
-)
-node backend/server.js
-exit /b %ERRORLEVEL%
